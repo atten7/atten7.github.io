793f6614be073817af359f01f302e79d2c7a38dc
atten7 <atten0914@gmail.com>
2025-08-04 15:08:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,13 +23,5 @@ def upload():
         return render_template('index.html', show_apikey_input=True)
     return 'pptx 파일만 업로드 가능합니다.'
 
-app.secret_key = 'your_secret_key'
-
-@app.route('/set_apikey', methods=['POST'])
-def set_apikey():
-    apikey = request.form.get('apikey')
-    session['gemini_apikey'] = apikey
-    return render_template('index.html', show_apikey_input=False, apikey_saved=True, apikey=apikey)
-
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
