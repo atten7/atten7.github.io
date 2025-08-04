2cd652908473b7310613f1747902e44dc230f37a
atten7 <atten0914@gmail.com>
2025-08-04 16:08:39 +0900

    Update main.py

--- talking-ppt/src/main.py
+++ talking-ppt/src/main.py
@@ -1,12 +1,13 @@
 from ppt_extractor import extract_ppt_content
 from script_generator import generate_presentation_script_with_context_ollama
 
-def main(filepath)
+def main(filepath):
     PPT_FILE_PATH = filepath
 
     extracted_data = extract_ppt_content(PPT_FILE_PATH)
 
     if extracted_data:
         generate_presentation_script_with_context_ollama(extracted_data)
+
 
 
