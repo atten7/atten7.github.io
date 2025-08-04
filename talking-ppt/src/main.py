9379712b37aa4b2d95331b394b790a60cdbaca63
atten7 <atten0914@gmail.com>
2025-08-04 16:00:06 +0900

    Update main.py

--- talking-ppt/src/main.py
+++ talking-ppt/src/main.py
@@ -2,9 +2,10 @@ from ppt_extractor import extract_ppt_content
 from script_generator import generate_presentation_script_with_context_ollama
 
 if __name__ == "__main__":
-    PPT_FILE_PATH = r"C:\Users\user\Desktop\2025 해커톤\talking-ppt\assets\10230_최민기 (진로 PPT).pptx"
+    PPT_FILE_PATH = filepath
 
     extracted_data = extract_ppt_content(PPT_FILE_PATH)
 
     if extracted_data:
         generate_presentation_script_with_context_ollama(extracted_data)
+
